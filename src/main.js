609c18a50e4dc63a2d9f710a9a1110528683c7e6
Bryan Hernandez <proanthonyhdez@gmail.com>
2021-06-20 01:13:17 -0600

    #

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import axios from 'axios'
 Vue.config.productionTip = false;
 //axios.defaults.baseURL="https://localhost:44370"
 //prod
+
 axios.defaults.baseURL="https://apiexpediente.herokuapp.com"
 new Vue({
   router,
