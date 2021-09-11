ec702957dbaf42a23e47fcd61e27392f8eb40aeb
guillermo samayoa <you@example.com>
2021-09-11 16:54:29 -0600

    exceltojsonbasico

--- src/views/IngresoEgreso.vue
+++ src/views/IngresoEgreso.vue
@@ -1,5 +1,54 @@
 <template>
-    <div>
-     ingreso y egreso   
-    </div>
+  <div>
+    <v-file-input
+    accept=".xls,.xlsx"
+    label="File input"
+    v-on:change="fileToJson($event)"
+    v-model="archivo">
+    </v-file-input>
+    <input type="file" name="xlfile" id="xlf" v-on:change="fileToJson($event)" />
+  </div>
 </template>
+
+<script>
+import XLSX from 'xlsx';
+//import { readExcel } from '../components/importarExcel'
+
+    export default {
+      
+      data() {
+        return {
+          archivo: null,
+        }
+      },
+      methods:{
+          
+         
+      fileToJson (e) {
+  const file = e.target.files[0]
+  /* Boilerplate to set up FileReader */
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    /* Parse data */
+    const bstr = e.target.result
+    const wb = XLSX.read(bstr, { type: 'binary' })
+    /* Get first worksheet */
+    const wsname = wb.SheetNames[0]
+    const ws = wb.Sheets[wsname]
+    /* Convert array of arrays */
+    const data = XLSX.utils.sheet_to_json(ws)
+    console.log(data)
+    
+    /* Update state */
+    //this.data = data
+    //const header = data.shift()
+    
+    
+  }
+  reader.readAsBinaryString(file)
+}
+    },
+      }
+    
+
+</script>
